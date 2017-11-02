db2f7fc708703c182aa28dee330796a3db08c2b4
Chitalkin <aleksandr.chitalkin@accenture.com>
2017-11-02 13:01:50 +0300

    new version

--- lchi.R
+++ lchi.R
@@ -1,16 +1,18 @@
-library(httr)
+library(rvest)
 library(XML)
+library(httr)
 library(data.table)
 library(dtplyr)
 library(dplyr)
 
 find_last_page <- function() {
   go_next <- TRUE
-
+  next_page <- 1
+  
   while(go_next) {
     doc <- read_lchi_page(next_page)
     pnav <- html_nodes(doc, css = '.pnav')
-
+    
     next_page <- gsub("^.+fChPage\\(([0-9]+)\\).+$", "\\1", 
                       pnav[grepl(pattern = "fChPage", pnav)], perl = T) %>% 
       as.integer() %>% unique() %>% max()
@@ -25,12 +27,12 @@ read_lchi_page <- function(x) {
   url <- "http://investor.moex.com/ru/statistics/2017/default.aspx"
   
   resp <- POST(url, body=list(act = '',
-                      sby = 8,
-                      nick = '',
-                      `data-contype-id-1` = '',
-                      `data-contype-id-2` = '',
-                      `data-contype-id-3` = '',
-                      pge = x), encode="form")
+                              sby = 8,
+                              nick = '',
+                              `data-contype-id-1` = '',
+                              `data-contype-id-2` = '',
+                              `data-contype-id-3` = '',
+                              pge = x), encode="form")
   doc <- content(resp)  
 }
 
@@ -45,29 +47,39 @@ read_all_ids <- function() {
 
 generate_js <- function(x) {
   js <- paste0("var webPage = require('webpage');
-var page = webPage.create();
-
-var fs = require('fs');
-var path = 'userpage.html'
-
-page.open('http://investor.moex.com", x,"', function (status) {
-  var content = page.content;
-  fs.write(path,content,'w')
-  phantom.exit();
-});")
+               var page = webPage.create();
+               
+               var fs = require('fs');
+               var path = 'userpage.html'
+               
+               page.open('http://investor.moex.com", x,"', function (status) {
+               var content = page.content;
+               fs.write(path,content,'w')
+               phantom.exit();
+               });")
   f <- file("scrape_lchi.js", "w")
   writeLines(js, con = f)
   close(f)
 }
 
 read_user_data_table <- function(url) {
+  print(url)
+  generate_js(url)
   system("./phantomjs scrape_lchi.js", show.output.on.console = F)
   df <- readHTMLTable(htmlParse("userpage.html"), stringsAsFactors = F)
   deals <- df[[length(df)]]
-  colnames(deals) <- c("MARKET", "SEC", "POSITION", "PRICE", "BALANCE", "COST")
-  v <- vars(POSITION, PRICE, BALANCE, COST)
-  as_tibble(deals) %>% mutate_at("POSITION", 
-                                 function(x) gsub("(\\d+).+", "\\1", x)) %>% 
-    mutate_at(v, function(x) gsub("-", "0", x)) %>% 
-    mutate_at(v, as.numeric)
+  if(!is.null(deals)) {
+    if((ncol(deals) == 6) & (nrow(deals) > 0)) {
+      colnames(deals) <- c("MARKET", "SEC", "POSITION", "PRICE", "BALANCE", "COST")
+      v <- vars(POSITION, PRICE, BALANCE, COST)
+      as_tibble(deals) %>% mutate_at("POSITION", 
+                                     function(x) gsub("(\\d+).+", "\\1", x)) %>% 
+        mutate_at(v, function(x) gsub("-", "0", x)) %>% 
+        mutate_at(v, as.numeric) %>% select(SEC, POSITION)
+    }    
+  }
+}
+
+read_all_users <- function() {
+  do.call("bind_rows", lapply(read_all_ids(), read_user_data_table))
 }
